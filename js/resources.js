8ff23bf8e5c11efa890052472481d1ed0c47422c
xiang <heroes618@hotmail.com>
2015-01-22 08:44:50 -0800

    video 4 commites

--- js/resources.js
+++ js/resources.js
@@ -4,8 +4,8 @@ game.resources = [
 	 * @example
 	 * {name: "example", type:"image", src: "data/img/example.png"},
 	 */
-    {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
-	{name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
+    {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},//awesomenauts/data/img...
+	{name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},//awesomenauts/data/img......
 	/* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
@@ -16,7 +16,7 @@ game.resources = [
 	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
 	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
  	 */
- 	 {name: "level01", type: "tmx", src: "data/map/test.tmx"},
+ 	 {name: "level01", type: "tmx", src: "data/map/test.tmx"},//awesomenauts/data/map.....
 
 	/* Background music. 
 	 * @example
