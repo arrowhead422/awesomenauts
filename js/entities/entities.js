66274aed6a8b4f69347c4dc7916bb0a6b5e53d53
xiang <heroes618@hotmail.com>
2015-01-27 08:35:35 -0800

    video 10 coded

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -15,7 +15,11 @@ game.PlayerEntity = me.Entity.extend({// game and me .Entity is a class
 
 	    this.body.setVelocity(5, 20); // this part sets the locating of the player
 
-	},
+	    this.renderable.addAnimation("idle", [78]);
+	    this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
+
+	    this.renderable.setCurrentAnimation("idle");
+	},   
 
 	update: function (delta){
 		if(me.input.isKeyPressed("right")){
@@ -23,13 +27,25 @@ game.PlayerEntity = me.Entity.extend({// game and me .Entity is a class
 			//seVelocity() and multiplying itby me.timer.tick.
 			//me.timer.tick makes the movement look smoot
 			this.body.vel.x += this.body.accel.x * me.timer.tick;
-
+			
+			this.flipX(true);
 		}
 		else{
 			this.body.vel.x = 0;
 		}
 
+		if(this.body.vel.x !== 0){
+
+		if(!this.renderable.isCurrentAnimation("walk")){
+			this.renderable.setCurrentAnimation("walk");
+		}
+	}else{
+		this.renderable.setCurrentAnimation("idle");
+	}
+
 		this.body.update(delta);
+
+		this._super(me.Entity, "update", [delta]);
 		return true;
 
 
