fc150eb63d7eb5cd047bf5a3d35deab0fa2ca1cc
heroes618 <heroes618@hotmail.com>
2015-02-19 10:05:26 -0700

    video 23 coded

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -124,16 +124,39 @@ game.PlayerEntity = me.Entity.extend({// game and me .Entity is a class
 			else if(xdif>-35 && this.facing==='right' && (xdif<0)){ // when player collide with enemybase it will stop the player from the right side
 				this.body.vel.x = 0;
 				this.pos.x = this.pos.x -1;
-			}else if (xdif<70 && this.facing==='left' && (xdif>0)) {// the left side
+			}else if (xdif<70 && this.facing==='left' && xdif>0) {// the left side
 				this.body.vel.x = 0;
 				this.pos.x = this.pos.x +1;
 
 			}
 
-			if(this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit >=1000){
+			if(this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit >= 1000){
 				console.log("tower Hit");
 				this.lastHit = this.now;
 				response.b.loseHealth();
+			}
+		}
+		else if (response.b.type==='EnemyCreep') {
+			var xdif = this.pos.x - response.b.pos.x;
+			var ydif = this.pos.y - response.b.pos.y;
+
+			if(xdif>0){
+				this.pos.x = this.pos.x + 1;
+				if (this.facing==="left") {
+					this.body.vel.x = 0;
+				}
+			}else{
+				this.pos.x = this.pos.x - 1;
+				if (this.facing==="right") {
+					this.body.vel.x = 0;
+			}
+}
+			if(this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit >= 1000
+				&& (Math.abs(ydif) <=40) && 
+				(((xdif>0) && this.facing==="left") || ((xdif<0) && this.facing==="right"))
+				){
+				this.lastHit = this.now;
+				response.b.loseHealth(1);
 			}
 		}
 			
@@ -270,7 +293,16 @@ game.EnemyCreep = me.Entity.extend({ // enemy team creep
 
 	},
 
+	loseHealth: function(damage){
+		this.health = this.health - damage;
+	},
+
 	update: function(delta){ //update the enemey creep
+		console.log(this.health);
+		
+		if (this.health <=0) {
+			me.game.world.removeChild(this);
+		}
 
 		this.now = new Date().getTime();
 
