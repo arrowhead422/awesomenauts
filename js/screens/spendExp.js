d77d6c9d8282d7af66a65ca4161252d1ee2a7834
heroes618 <heroes618@hotmail.com>
2015-03-12 22:47:34 -0700

    video 43 finish with no error

--- js/screens/spendExp.js
+++ js/screens/spendExp.js
@@ -8,13 +8,17 @@ game.SpendExp = me.ScreenObject.extend({
 
  		me.game.world.addChild(new (me.Renderable.extend ({ // % this game world controls the meau
  			init: function(){
- 				this._super(me.Renderable, "init", [270, 240, 300, 50]);
+ 				this._super(me.Renderable, "init", [10, 10, 300, 50]);
  				this.font = new me.Font("Arial", 46, "white");
  			},
 
  			draw: function(renderer){
-				this.font.draw(renderer.getContext(), "SPEND", this.pos.x, this.pos.y);
-				
+				this.font.draw(renderer.getContext(), "PRESS F1-F4 TO BUY, F5 to SKIP", this.pos.x, this.pos.y);
+				this.font.draw(renderer.getContext(), "CURRENT EXP:" + game.data.exp.toString(), this.pos.x + 100, this.pos.y + 50);
+				this.font.draw(renderer.getContext(), "F1: Increase Gold production" + game.data.exp.toString(), this.pos.x + 200, this.pos.y + 100);
+				this.font.draw(renderer.getContext(), "F2: Add Starting Gold" + game.data.exp.toString(), this.pos.x + 200, this.pos.y + 150);
+				this.font.draw(renderer.getContext(), "F3: Increase Damage" + game.data.exp.toString(), this.pos.x + 200, this.pos.y + 200);
+				this.font.draw(renderer.getContext(), "F4: Increase Health" + game.data.exp.toString(), this.pos.x + 200, this.pos.y + 250);
 			}
 
 			
