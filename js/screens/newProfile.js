6f4d11d6d3ff00b33fc4c7775e590be6c541bad3
heroes618 <heroes618@hotmail.com>
2015-03-28 23:52:02 -0700

    video 52 finish

---
+++ js/screens/newProfile.js
@@ -0,0 +1,41 @@
+game.NewProfile = me.ScreenObject.extend({
+	/**	
+	 *  action to perform on state change
+	 */
+	onResetEvent: function() {	
+
+		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('new-screen')), -10);		 
+
+		me.input.unbindKey(me.input.KEY.F1.B);
+		me.input.unbindKey(me.input.KEY.F2.Q);
+		me.input.unbindKey(me.input.KEY.F3.E);
+		me.input.unbindKey(me.input.KEY.F4.W);
+		me.input.unbindKey(me.input.KEY.F5.A); 
+
+ 		me.game.world.addChild(new (me.Renderable.extend ({ // % this game world controls the meau
+ 			init: function(){
+ 				this._super(me.Renderable, "init", [10, 10, 300, 50]);
+ 				this.font = new me.Font("Arial", 26, "white");
+ 			},
+
+ 			draw: function(renderer){
+				this.font.draw(renderer.getContext(), "PICK A UserName And PassWord", this.pos.x, this.pos.y);
+				
+			}
+
+			
+		})));
+
+		
+},
+
+
+
+	/**	
+	 *  action to perform when leaving this screen (state change)
+	 */
+	onDestroyEvent: function() {
+
+		
+	}
+});
